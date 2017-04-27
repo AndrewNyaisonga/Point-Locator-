8102fb9b4c735201302c66d077f63f6369efc3e0
Andrew Nyaisonga <anyaison@u.rochester.edu>
2017-04-27 14:50:40 -0400

    Add files via upload

---
+++ Point.java
@@ -0,0 +1,13 @@
+/**
+ * Created by andrewnyaisonga on 3/30/17.
+ */
+public class Point {
+
+    double x;
+    double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+}
